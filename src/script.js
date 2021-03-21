08e5e9666a7a9341a26ab2b6df4d02afb49ca741
sophiacheong <sophiaacheong@gmail.com>
2021-03-20 20:30:58 -0700

    tested out get all request for postgres and mongo

--- src/script.js
+++ src/script.js
@@ -3,11 +3,44 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '2m', target: 100 }, // below normal load
+    { duration: '5m', target: 100 },
+    { duration: '2m', target: 200 }, // normal load
+    { duration: '5m', target: 200 },
+    { duration: '2m', target: 300 }, // around the breaking point
+    { duration: '5m', target: 300 },
+    { duration: '2m', target: 400 }, // beyond the breaking point
+    { duration: '5m', target: 400 },
+    { duration: '10m', target: 0 }, // scale down. Recovery stage.
+  ],
 };
 
 export default function () {
-  http.get('http://localhost:8080/api/products?page=1&count=10');
+  const BASE_URL = 'http://localhost:8080/api'; // make sure this is not production
+
+  const responses = http.batch([
+    [
+      'GET',
+      `${BASE_URL}/products`,
+      null,
+    ],
+    [
+      'GET',
+      `${BASE_URL}/products?page=1&count=10`,
+      null,
+    ],
+    [
+      'GET',
+      `${BASE_URL}/products?page=2&count=5`,
+      null,
+    ],
+    [
+      'GET',
+      `${BASE_URL}/products?page=2&count=10`,
+      null,
+    ],
+  ]);
+
   sleep(1);
 }
