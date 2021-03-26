7bb0137f4bce5c78d3de994e512eae47d0e5e12c
sophiacheong <sophiaacheong@gmail.com>
2021-03-25 19:24:43 -0700

    added 2 more server instances, have 4 in total now

---
+++ login/server3.sh
@@ -0,0 +1,1 @@
+ssh -i "../pem/SDC-DB.pem" ubuntu@ec2-13-56-188-167.us-west-1.compute.amazonaws.com
---
+++ login/server4.sh
@@ -0,0 +1,1 @@
+ssh -i "../pem/SDC-DB.pem" ubuntu@ec2-54-219-183-168.us-west-1.compute.amazonaws.com
